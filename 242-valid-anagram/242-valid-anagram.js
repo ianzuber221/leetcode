621e1142542f837ebe77ec6ce7f58bd36bb9206c
Ian Zuber <ianzuber221@gmail.com>
2022-08-30 10:56:56 -0400

    Time: 139 ms (35.76%), Space: 43.9 MB (60.99%) - LeetHub

---
+++ 242-valid-anagram/242-valid-anagram.js
@@ -0,0 +1,28 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isAnagram = function(s, t) {
+    //check if strings are same size if not return false
+    if(s.length !== t.length) return false;
+    // iterate through s put into hash table
+    let hash = {};
+    for(const letter of s){
+        !hash[letter] ? hash[letter] = 1 : hash[letter]++
+    }
+    // iterate through t check hash table
+    for (const letter of t){
+        if(!hash[letter]){
+            return false;
+        }
+        hash[letter]--;
+    }
+    // if letter is not in hash table return false
+    //check hash table for values that arent 0 and if found return false
+    for(const key in hash){
+        if(hash[key] !== 0) return false;
+    }
+    //else return true
+    return true;
+};
