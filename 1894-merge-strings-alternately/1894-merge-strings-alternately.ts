c8f91334eea2c4659791382be033419af8df41c4
Ian Zuber <ianzuber221@gmail.com>
2025-02-26 20:14:39 -0500

    Time: 58 ms (59.86%), Space: 56.3 MB (5.84%) - LeetHub

---
+++ 1894-merge-strings-alternately/1894-merge-strings-alternately.ts
@@ -0,0 +1,11 @@
+function mergeAlternately(word1: string, word2: string): string {
+    let i = 0, j=0, result = [];
+    while(i < word1.length || j < word2.length){
+        const num1 = word1[i], num2 = word2[j];
+        if(num1) result.push(num1);
+        if(num2) result.push(num2);
+        i++;
+        j++;
+    }
+    return result.join('');
+};
