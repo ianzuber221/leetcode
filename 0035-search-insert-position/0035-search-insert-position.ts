eec94ff28d5f0c8c7e316d2eb3cbbbc96b16b70b
Ian Zuber <ianzuber221@gmail.com>
2025-03-04 02:06:19 -0500

    Time: 3 ms (6.64%), Space: 58.7 MB (5.95%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.ts
+++ 0035-search-insert-position/0035-search-insert-position.ts
@@ -1,7 +1,6 @@
 function searchInsert(nums: number[], target: number): number {
 for (const [ind, num] of nums.entries()) {        
-        if(num > target)return ind 
-        if(num === target) return ind
+        if(num >= target)return ind 
     }
     return nums.length 
 };
