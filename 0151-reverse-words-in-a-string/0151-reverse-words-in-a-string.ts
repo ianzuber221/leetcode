6c857e776a389cc261aa3da1053330e60f289152
Ian Zuber <ianzuber221@gmail.com>
2025-03-11 23:23:18 -0400

    Time: 7 ms (10.1%), Space: 59.5 MB (5.44%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.ts
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.ts
@@ -1,15 +1,14 @@
 function reverseWords(s: string): string {
     const resultArr= [] 
     let word = []
-    for(let i = s.length-1; i>=0; i--){
-        const char = s[i]
+    for(const char of s){
         if(char != ' '){
-            word.unshift(char)
+            word.push(char)
         }else if(word.length){
             resultArr.push(word.join(''))
             word =[]
         }
     }
     if(word.length)  resultArr.push(word.join(''))
-    return resultArr.join(' ')
+    return resultArr.reverse().join(' ')
 };
