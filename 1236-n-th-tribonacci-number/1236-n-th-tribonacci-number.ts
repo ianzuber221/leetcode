c3d15dc9e841532d4e9bf6f704dde4c12da61450
Ian Zuber <ianzuber221@gmail.com>
2025-03-14 05:00:15 -0400

    Time: 6 ms (3.02%), Space: 57.4 MB (6.51%) - LeetHub

---
+++ 1236-n-th-tribonacci-number/1236-n-th-tribonacci-number.ts
@@ -0,0 +1,24 @@
+function tribonacci(n: number): number {
+    let t0 = 0, t1 = 1, t2 = 1
+    switch(n){
+        case 0:
+            return 0
+            break;
+        case 1:
+            return 1
+            break;
+        case 2:
+            return 1;
+            break;
+    }
+    const recurse = (num = 3) => {
+        let trib = t0 + t1 + t2
+        if(num === n) return trib
+        console.log(trib)
+        t0 = t1
+        t1 = t2
+        t2 = trib
+        return recurse(num + 1)
+    }
+    return recurse()
+};
